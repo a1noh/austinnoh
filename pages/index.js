c70cd33c19c4249090ffd6d5a59e3fb45067026e
austin noh <noh1austin@gmail.com>
2021-02-06 23:58:20 +0900

    css and images

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,47 @@
 import React from "react";
 import BaseLayout from "../components/layouts/BaseLayout";
-import { Button, Container } from "reactstrap";
+import { Container, Row, Col } from "reactstrap";
 
 class Index extends React.Component {
   render() {
     return (
-      <BaseLayout>
-        <Container>
-          <Button color="danger">Danger!</Button>
-        </Container>
+      <BaseLayout className="cover">
+        <div className="main-section">
+          <div className="background-image">
+            <img src="/images/background-index.png" />
+          </div>
+
+          <Container>
+            <Row>
+              <Col md="6">
+                <div className="hero-section">
+                  <div className={`flipper`}>
+                    <div className="back">
+                      <div className="hero-section-content">
+                        <h2> CS Student at Purdue University </h2>
+                        <div className="hero-section-content-intro">
+                          Have a look at my portfolio and job history.
+                        </div>
+                      </div>
+                      <img className="image" src="/images/section-1.png" />
+                      <div className="shadow-custom">
+                        <div className="shadow-inner"> </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </Col>
+              <Col md="6" className="hero-welcome-wrapper">
+                <div className="hero-welcome-text">
+                  <h1>Hello, I am</h1>
+                </div>
+                <div className="hero-welcome-bio">
+                  <h1>AUSTIN NOH</h1>
+                </div>
+              </Col>
+            </Row>
+          </Container>
+        </div>
       </BaseLayout>
     );
   }
