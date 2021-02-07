bd34252b14a2ae001321dcb4932f3f9eb046f18f
austin noh <noh1austin@gmail.com>
2021-02-07 22:59:51 +0900

    react typed

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
 import React from "react";
 import BaseLayout from "../components/layouts/BaseLayout";
 import { Container, Row, Col } from "reactstrap";
+import Typed from "react-typed";
 
+const ROLES = ["Software Engineer", "Psych Student", "CS Student"];
 class Index extends React.Component {
   render() {
     return (
@@ -35,6 +37,17 @@ class Index extends React.Component {
                 <div className="hero-welcome-text">
                   <h1>Hello, I am</h1>
                 </div>
+                <Typed
+                  loop
+                  strings={ROLES}
+                  typeSpeed={70}
+                  backSpeed={70}
+                  backDelay={1000}
+                  loopCount={0}
+                  showCursor
+                  className="self-typed"
+                  cursorChar="|"
+                ></Typed>
                 <div className="hero-welcome-bio">
                   <h1>AUSTIN NOH</h1>
                 </div>
