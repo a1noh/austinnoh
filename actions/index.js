bf634d5d885e28e3adf1316eddbccc081b687233
austin noh <noh1austin@gmail.com>
2021-02-18 21:34:10 +0900

    revert to previous

--- actions/index.js
+++ actions/index.js
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 export const useGetPosts = () => {
   const [posts, setPosts] = useState([]);
   const [error, setError] = useState();
+
   useEffect(() => {
     async function getPosts() {
       const res = await fetch("/api/v1/posts");
