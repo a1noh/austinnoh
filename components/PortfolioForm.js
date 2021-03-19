b7fba2b2cc811adc9d9458b2c965a85c1559e6e0
austin noh <noh1austin@gmail.com>
2021-03-19 18:40:03 +0900

    enable/disable end date

--- components/PortfolioForm.js
+++ components/PortfolioForm.js
@@ -103,11 +103,37 @@ const PortfolioForm = ({ onSubmit }) => {
         <div>
           <DatePicker
             showYearDropdown
+            disabled={!endDate}
             className=" form-control port-input"
             selected={endDate}
             onChange={handleDateChange("endDate", setEndDate)}
           />
         </div>
+      </div>
+      <div className="form-group">
+        {endDate && (
+          <button
+            type="button"
+            className="btn btn-danger"
+            onClick={() => handleDateChange("endDate", setEndDate)(null)}
+          >
+            No End Date
+          </button>
+        )}
+        {!endDate && (
+          <button
+            type="button"
+            className="btn btn-success"
+            onClick={() =>
+              handleDateChange(
+                "endDate",
+                setEndDate
+              )(new Date(new Date().setHours(0, 0, 0, 0)))
+            }
+          >
+            Set End Date
+          </button>
+        )}
       </div>
       <button type="submit" className="btn btn-primary">
         Create
