ec2f005d1763621bfa8d68a030616b65d2c1ab67
austin noh <noh1austin@gmail.com>
2021-03-25 23:33:48 +0900

    generate access token during session

--- pages/api/v1/portfolios.js
+++ pages/api/v1/portfolios.js
@@ -1,7 +1,10 @@
 import PortfolioApi from "@/lib/api/portfolios";
+import auth0 from "@/utils/auth0";
+
 export default async function createPortfolio(req, res) {
   try {
-    const data = req.body;
+    const { accessToken } = await auth0.getSession(req);
+    console.log(accessToken);
     await new PortfolioApi().createPortfolio(data);
     return res.json({ message: "Portfolio was created!" });
   } catch (e) {
