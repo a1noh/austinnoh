6f6603bfbc2ad341830c12f3ba1b79ce0821334c
austin noh <noh1austin@gmail.com>
2021-04-02 00:42:31 +0900

    handle errors

--- pages/portfolios/new.js
+++ pages/portfolios/new.js
@@ -1,28 +1,28 @@
-import BaseLayout from "@/components/layouts/BaseLayout";
-import BasePage from "@/components/BasePage";
-import withAuth from "@/hoc/withAuth";
-import { Row, Col } from "reactstrap";
-import PortfolioFrom from "@/components/PortfolioForm";
+import BaseLayout from '@/components/layouts/BaseLayout';
+import BasePage from '@/components/BasePage';
+import withAuth from '@/hoc/withAuth';
+import { Row, Col } from 'reactstrap';
+import PortfolioForm from '@/components/PortfolioForm';
 import { useCreatePortfolio } from '@/actions/portfolios';
+import Redirect from '@/components/shared/Redirect';
 
-const PortfolioNew = ({ user, loading: userLoading }) => {
+const PortfolioNew = ({user, loading: userLoading}) => {
   const [createPortfolio, {data, loading, error}] = useCreatePortfolio();
-  debugger;
-  const _createPortfolio = (data) => {
-    createPortfolio(data);
-  };
+
+  if (data) { return <Redirect to="/portfolios" /> }
 
   return (
     <BaseLayout user={user} loading={userLoading}>
-      <BasePage>
+      <BasePage header="Create Portfolio">
         <Row>
           <Col md="8">
-            <PortfolioFrom onSubmit={_createPortfolio}></PortfolioFrom>
+            <PortfolioForm onSubmit={createPortfolio} />
+            { error && <div className="alert alert-danger mt-2">{error}</div>}
           </Col>
         </Row>
       </BasePage>
     </BaseLayout>
-  );
-};
+  )
+}
 
-export default withAuth(PortfolioNew)("admin");
+export default withAuth(PortfolioNew)('admin');
