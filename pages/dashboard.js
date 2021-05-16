eba017ab544d63f5dba86ba70a5adb77757a6dc1
austin noh <noh1austin@gmail.com>
2021-05-16 16:41:03 -0400

    fetch blogs by user

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,13 +1,17 @@
 import BaseLayout from "@/components/layouts/BaseLayout";
 import BasePage from "@/components/BasePage";
-import withAuth from "@/hoc/withAuth";
+import { withAuth } from "@/utils/auth0";
 import { Row, Col } from "reactstrap";
 import Masthead from "@/components/shared/Masthead";
 
-const Dashboard = ({ user, loading }) => {
+import auth0 from "@/utils/auth0";
+import BlogApi from "@/lib/api/blogs";
+
+const Dashboard = ({ user, blogs }) => {
+  debugger;
   return (
     <BaseLayout navClass="transparent" user={user} loading={false}>
-      <Masthead imagePath="images/home-bg.jpg" />
+      <Masthead imagePath="/images/home-bg.jpg" />
       <BasePage className="blog-user-page">
         <Row>
           <Col md="6" className="mx-auto text-center">
@@ -22,4 +26,9 @@ const Dashboard = ({ user, loading }) => {
   );
 };
 
-export default withAuth(Dashboard)("admin");
+export const getServerSideProps = withAuth(async ({ req, res }) => {
+  const { accessToken } = await auth0.getSession(req);
+  const json = await new BlogApi(accessToken).getByUser();
+  return { blogs: json.data };
+})("admin");
+export default Dashboard;
