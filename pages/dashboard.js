efbe71cd33846989de4a738a4563669dd6632b59
austin noh <noh1austin@gmail.com>
2021-05-16 18:02:34 -0400

    dashboard dropdowns

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -3,12 +3,51 @@ import BasePage from "@/components/BasePage";
 import { withAuth } from "@/utils/auth0";
 import { Row, Col } from "reactstrap";
 import Masthead from "@/components/shared/Masthead";
+import PortDropdown from "@/components/shared/Dropdown";
+import Link from "next/link";
 
 import auth0 from "@/utils/auth0";
 import BlogApi from "@/lib/api/blogs";
 
 const Dashboard = ({ user, blogs }) => {
-  debugger;
+  const createOptions = (blog) => {
+    return [
+      {
+        key: `${blog._id}-published`,
+        text: "Published",
+        handlers: {
+          onClick: () => {
+            alert(`Clicking Publish! ${blog._id}`);
+          },
+        },
+      },
+      {
+        key: `${blog._id}-delete`,
+        text: "Delete",
+        handlers: {
+          onClick: () => {
+            alert(`Clicking Delete! ${blog._id}`);
+          },
+        },
+      },
+    ];
+  };
+
+  const renderBlogs = (blogs, status) => (
+    <ul className="user-blogs-list">
+      {blogs
+        .filter((blog) => blog.status === status)
+        .map((blog) => (
+          <li key={blog._id}>
+            <Link href="/blogs/editor/[id]" as={`/blogs/editor/${blog._id}`}>
+              <a>{blog.title}</a>
+            </Link>
+            <PortDropdown items={createOptions(blog)} />
+          </li>
+        ))}
+    </ul>
+  );
+
   return (
     <BaseLayout navClass="transparent" user={user} loading={false}>
       <Masthead imagePath="/images/home-bg.jpg" />
@@ -16,9 +55,11 @@ const Dashboard = ({ user, blogs }) => {
         <Row>
           <Col md="6" className="mx-auto text-center">
             <h2 className="blog-status-title"> Published Blogs </h2>
+            {renderBlogs(blogs, "published")}
           </Col>
           <Col md="6" className="mx-auto text-center">
             <h2 className="blog-status-title"> Draft Blogs </h2>
+            {renderBlogs(blogs, "draft")}
           </Col>
         </Row>
       </BasePage>
