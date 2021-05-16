93b6dd899b5209bb327ebcd81fa1161429defd60
austin noh <noh1austin@gmail.com>
2021-05-16 14:32:34 -0400

    update blog on client and redirect

--- pages/blogs/editor/index.js
+++ pages/blogs/editor/index.js
@@ -2,16 +2,28 @@ import BaseLayout from "@/components/layouts/BaseLayout";
 import BasePage from "@/components/BasePage";
 import withAuth from "@/hoc/withAuth";
 import { Editor } from "slate-simple-editor";
+import { useCreateBlog } from "@/actions/blogs";
+import { toast } from "react-toastify";
+import { useRouter } from "next/router";
 
 const BlogEditor = ({ user, loading }) => {
-  const saveBlog = (data) => {
-    console.log(data);
+  const router = useRouter();
+  const [createBlog, { data: createdBlog, error, loading: blogLoading }] =
+    useCreateBlog();
+
+  const saveBlog = async (data) => {
+    const createdBlog = await createBlog(data);
+    router.push("/blogs/editor/[id]", `/blogs/editor/${createdBlog._id}`);
   };
+
+  if (error) {
+    toast.error(error.message);
+  }
 
   return (
     <BaseLayout user={user} loading={loading}>
       <BasePage>
-        {typeof window !== "undefined" && <Editor onSave={saveBlog} />}
+        <Editor onSave={saveBlog} loading={blogLoading} />
       </BasePage>
     </BaseLayout>
   );
