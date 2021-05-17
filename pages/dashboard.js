644abc860a2c91c47e07bc539d01b8ea93dee60e
austin noh <noh1austin@gmail.com>
2021-05-17 05:32:26 -0400

    show blogs on blog page

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,33 +1,47 @@
 import BaseLayout from "@/components/layouts/BaseLayout";
 import BasePage from "@/components/BasePage";
-import { withAuth } from "@/utils/auth0";
-import { Row, Col } from "reactstrap";
+import withAuth from "@/hoc/withAuth";
+import { Row, Col, Button } from "reactstrap";
 import Masthead from "@/components/shared/Masthead";
 import PortDropdown from "@/components/shared/Dropdown";
 import Link from "next/link";
-
+import { useUpdateBlog, useGetUserBlogs } from "@/actions/blogs";
+import { toast } from "react-toastify";
 import auth0 from "@/utils/auth0";
 import BlogApi from "@/lib/api/blogs";
 
-const Dashboard = ({ user, blogs }) => {
+const Dashboard = ({ user, loading }) => {
+  const [updateBlog] = useUpdateBlog();
+  const { data: blogs, mutate } = useGetUserBlogs();
+
+  const changeBlogStatus = async (blogId, status) => {
+    updateBlog(blogId, { status })
+      .then(() => mutate())
+      .catch(() => toast.error("Something went wrong..."));
+  };
+
+  const createOption = (blogStatus) => {
+    return blogStatus === "draft"
+      ? { view: "Publish Story", value: "published" }
+      : { view: "Make Draft", value: "draft" };
+  };
+
   const createOptions = (blog) => {
+    const option = createOption(blog.status);
+
     return [
       {
         key: `${blog._id}-published`,
-        text: "Published",
+        text: option.view,
         handlers: {
-          onClick: () => {
-            alert(`Clicking Publish! ${blog._id}`);
-          },
+          onClick: () => changeBlogStatus(blog._id, option.value),
         },
       },
       {
         key: `${blog._id}-delete`,
         text: "Delete",
         handlers: {
-          onClick: () => {
-            alert(`Clicking Delete! ${blog._id}`);
-          },
+          onClick: () => changeBlogStatus(blog._id, "deleted"),
         },
       },
     ];
@@ -35,22 +49,31 @@ const Dashboard = ({ user, blogs }) => {
 
   const renderBlogs = (blogs, status) => (
     <ul className="user-blogs-list">
-      {blogs
-        .filter((blog) => blog.status === status)
-        .map((blog) => (
-          <li key={blog._id}>
-            <Link href="/blogs/editor/[id]" as={`/blogs/editor/${blog._id}`}>
-              <a>{blog.title}</a>
-            </Link>
-            <PortDropdown items={createOptions(blog)} />
-          </li>
-        ))}
+      {blogs &&
+        blogs
+          .filter((blog) => blog.status === status)
+          .map((blog) => (
+            <li key={blog._id}>
+              <Link href="/blogs/editor/[id]" as={`/blogs/editor/${blog._id}`}>
+                <a>{blog.title}</a>
+              </Link>
+              <PortDropdown items={createOptions(blog)} />
+            </li>
+          ))}
     </ul>
   );
 
   return (
-    <BaseLayout navClass="transparent" user={user} loading={false}>
-      <Masthead imagePath="/images/home-bg.jpg" />
+    <BaseLayout navClass="transparent" user={user} loading={loading}>
+      <Masthead imagePath="/images/home-bg.jpg">
+        <h1>Blogs Dashboard</h1>
+        <span className="subheading">
+          Let's write some nice blog today{" "}
+          <Link href="/blogs/editor">
+            <Button color="primary">Create a new Blog</Button>
+          </Link>
+        </span>
+      </Masthead>
       <BasePage className="blog-user-page">
         <Row>
           <Col md="6" className="mx-auto text-center">
@@ -67,9 +90,4 @@ const Dashboard = ({ user, blogs }) => {
   );
 };
 
-export const getServerSideProps = withAuth(async ({ req, res }) => {
-  const { accessToken } = await auth0.getSession(req);
-  const json = await new BlogApi(accessToken).getByUser();
-  return { blogs: json.data };
-})("admin");
-export default Dashboard;
+export default withAuth(Dashboard)("admin");
